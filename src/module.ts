ce6c044ab940b1351e69de77da82b08597909ca9
Oskar Lebuda <o.lebuda@gmail.com>
2023-02-27 19:02:02 +0000

    chore(init): add required packages

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,7 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit';
 
 // Module options TypeScript inteface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {};
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -11,9 +11,9 @@ export default defineNuxtModule<ModuleOptions>({
   // Default configuration options of the Nuxt module
   defaults: {},
   setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
+    const resolver = createResolver(import.meta.url);
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+    addPlugin(resolver.resolve('./runtime/plugin'));
+  },
+});
